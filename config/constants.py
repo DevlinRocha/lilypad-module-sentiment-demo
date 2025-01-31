c90963e410db92548a9453c11a11b427e28603e5
Devlin <devlin@devlinrocha.com>
2025-01-31 13:28:59 -0800

    initial commit

---
+++ config/constants.py
@@ -0,0 +1,4 @@
+MODULE_REPO = ""
+TARGET_COMMIT = ""
+DOCKER_REPO = "devlin/lilypad-module-sentiment-demo"
+WEB3_DEVELOPMENT_KEY = ""
---
+++ src/run_inference.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import traceback
+import json
+
+
+import torch
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+
+
+def run_job(input, model, tokenizer):
+    """
+    Run the job
+    """
+    try:
+        inputs = tokenizer(
+            input,
+            return_tensors="pt",
+        )
+
+        with torch.no_grad():
+            logits = model(**inputs).logits
+
+        predicted_class_id = logits.argmax().item()
+        output = {
+            "input": input,
+            "sentiment": model.config.id2label[predicted_class_id],
+        }
+
+        return output
+
+    except Exception as error:
+        print(
+            f"❌ Error running job: {error}",
+            file=sys.stderr,
+            flush=True,
+        )
+        traceback.print_exc(file=sys.stderr)
+        raise
+
+
+def main():
+    print("Starting inference...")
+
+    input = os.environ.get("INPUT", "Default input value")
+    model_directory = os.environ.get("MODEL_DIRECTORY", "/models")
+
+    output = {"input": input, "status": "error"}
+
+    try:
+        tokenizer = DistilBertTokenizer.from_pretrained(model_directory)
+        model = DistilBertForSequenceClassification.from_pretrained(model_directory)
+
+        output = run_job(input, model, tokenizer)
+        # output.update(
+        #     {
+        #         "status": "success",
+        #     }
+        # )
+
+    except Exception as error:
+        print("❌ Error during processing:", file=sys.stderr, flush=True)
+        traceback.print_exc(file=sys.stderr)
+        output["error"] = str(error)
+
+    os.makedirs("/outputs", exist_ok=True)
+    output_path = "/outputs/result.json"
+
+    try:
+        with open(output_path, "w") as f:
+            json.dump(output, f, indent=2)
+        print(
+            f"✅ Successfully wrote output to {output_path}",
+        )
+    except Exception as error:
+        print(f"❌ Error writing output file: {error}", file=sys.stderr, flush=True)
+        traceback.print_exc(file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
